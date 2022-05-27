0dcc24bfce383e9c1350ad4b61ba5bbbd2d3ebd7
Ashutosh Khot <ashutoshkhot10@gmail.com>
2022-05-27 17:17:31 +0530

    Demo.java created

---
+++ Demo.java
@@ -0,0 +1,25 @@
+class Array
+{
+	public static void main(String args[])
+	{
+		int n;
+		int arr = new int[n];
+		
+		System.out.println("Enter the size of Array");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		System.out.println("Enter the elements of Array");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		System.out.println("Display the Array")
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
+
